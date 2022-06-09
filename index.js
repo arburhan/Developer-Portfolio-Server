9589793392c0c2bbb6733fb3554aa5a58543c3e3
arburhan <buthan190629@outlook.com>
2022-06-09 23:47:05 +0600

    successfully connect to mongodb

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+// mongodb user
+const uri = `mongodb+srv://${process.env.DB_USER}:{${process.env.DB_PASS}@cluster0.wijkb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log('hello mongodb')
+    // perform actions on the collection object
+    client.close();
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
