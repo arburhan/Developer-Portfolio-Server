6ebd73b39faef6241397d18a74375ca042414b7a
arburhan <buthan190629@outlook.com>
2022-06-10 22:33:18 +0600

    error

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 // mongodb user
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wijkb.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://adminPortfolioAR:L1a3JKjIy55WYP6P@cluster0.wijkb.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri)
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
